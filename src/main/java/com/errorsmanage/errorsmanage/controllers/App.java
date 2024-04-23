acdf079fedb439aa5ac69fcbf2e5f07d4cd55aa1
LuisMunar <luis.emunarr@gmail.com>
2024-04-22 19:10:39 -0500

    Video 83 ready

--- src/main/java/com/errorsmanage/errorsmanage/controllers/App.java
+++ src/main/java/com/errorsmanage/errorsmanage/controllers/App.java
@@ -3,19 +3,25 @@ package com.errorsmanage.errorsmanage.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/app")
 public class App {
   @GetMapping("")
-  public String index() {
-    int value = 1 / 0;
-    System.out.println(value);
-    return "Greetings from Spring Boot!";
+  public ResponseEntity<Integer> index() {
+    // int value = 1 / 0;
+    // return ResponseEntity.status(HttpStatus.OK).body(value);
+
+    int value = Integer.parseInt("10x");
+    return ResponseEntity.status(HttpStatus.OK).body(value);
+    
     // try {
-    //   throw new Exception("This is an exception");
+    //   int value = 1 / 0;
+    //   return ResponseEntity.status(HttpStatus.ACCEPTED).body(value);
     // } catch (Exception e) {
-    //   return e.getMessage();
+    //   return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
     // }
   }
 }
--- src/main/java/com/errorsmanage/errorsmanage/controllers/HandlerException.java
+++ src/main/java/com/errorsmanage/errorsmanage/controllers/HandlerException.java
@@ -3,9 +3,12 @@ package com.errorsmanage.errorsmanage.controllers;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.ResponseEntity;
-import java.util.Date;
 import org.springframework.http.HttpStatus;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.errorsmanage.errorsmanage.models.Error;
 
@@ -22,5 +25,14 @@ public class HandlerException {
   public ResponseEntity<Error> notFound(Exception e) {
     Error error = new Error("Not Found", e.getMessage(), new Date());
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+  }
+
+  @ExceptionHandler({ NumberFormatException.class })
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public Map<String, String> numberFormatException(Exception e) {
+    Map<String, String> error = new HashMap<>();
+    error.put("error", "Number Format Exception");
+    error.put("message", e.getMessage());
+    return error;
   }
 }
